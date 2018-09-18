6f6b9a4f059390d1a62750ab33ebde4a360d1a74
Chris Maltais <chris.maltais@hotmail.ca>
2018-09-17 22:13:16 -0400

    Add initial home layout, no links

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    var typed = new Typed('#typedjs', {
+          strings: ["student @ queen's university.^2000", "24th in the world at Call of Duty: Black Ops.^2000", "competitor.^2000", "lifelong learner.^2000", "software engineer.^2000"],
+          typeSpeed:70,
+          backDelay:60,
+    });
+});
