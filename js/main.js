f840b7500b5180b22679ee84d7fbf42db179b895
Chris Maltais <chris.maltais@hotmail.ca>
2018-09-24 12:57:34 -0400

    Change resume icon, add tooltip

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var typed = new Typed('#typedjs', {
-          strings: ["student @ queen's university.^2000", "24th in the world at Call of Duty: Black Ops.^2000", "competitor.^2000", "lifelong learner.^2000", "software engineer.^2000"],
+          strings: ["student @ Queen's University.^2000", "24th in the world at Call of Duty: Black Ops.^2000", "competitor.^2000", "lifelong learner.^2000", "software engineer.^2000"],
           typeSpeed:70,
           backDelay:60,
     });
